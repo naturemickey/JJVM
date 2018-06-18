8fc579bc0324d414c5ec0320341a2514125edf6a
Michael Chow <mickey_zhouyou@yeah.net>
2018-06-18 22:50:24 +0800

    do_commit

--- src/Cmd.java
+++ src/Cmd.java
@@ -1,14 +1,26 @@
 public class Cmd {
     public boolean helpFlag;
     public boolean versionFlag;
-    public String cpOption;
+    public String cpOption = "";
     public String clazz;
     public String[] args;
 
     public static Cmd parseCmd(String[] args) {
         Cmd cmd = new Cmd();
 
+        for (int i = 1; i < args.length; ++i) {
+            String arg = args[i];
+            if (arg.equals("-help") || arg.equals("-?")) {
+                cmd.helpFlag = true;
+            } else if (args.equals("-version")) {
+                cmd.versionFlag = true;
+            } else if (arg.equals("-cp") || arg.equals("-classpath")) {
+                
+            }
+        }
+
         cmd.clazz = args[0];
+
 
         return cmd;
     }
