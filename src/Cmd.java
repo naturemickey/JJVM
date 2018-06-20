19b4915b8beda516c10aa76e16a4fd39ffce79eb
周游(Michael Zhou)-顺丰科技 <089239@SF00089269MACA.local>
2018-06-20 09:54:58 +0800

    complete_ch01

--- src/Cmd.java
+++ src/Cmd.java
@@ -3,24 +3,30 @@ public class Cmd {
     public boolean versionFlag;
     public String cpOption = "";
     public String clazz;
-    public String[] args;
+    public String[] args = {};
 
     public static Cmd parseCmd(String[] args) {
         Cmd cmd = new Cmd();
 
-        for (int i = 1; i < args.length; ++i) {
+        for (int i = 0; i < args.length; ++i) {
             String arg = args[i];
             if (arg.equals("-help") || arg.equals("-?")) {
                 cmd.helpFlag = true;
-            } else if (args.equals("-version")) {
+            } else if (arg.equals("-version")) {
                 cmd.versionFlag = true;
             } else if (arg.equals("-cp") || arg.equals("-classpath")) {
-                
+                arg = args[++i];
+                cmd.cpOption = arg;
             }
         }
 
-        cmd.clazz = args[0];
-
+        if (args.length > 0) {
+            cmd.clazz = args[0];
+            cmd.args = new String[args.length - 1];
+            for (int i = 1; i < args.length; ++i) {
+                cmd.args[i - 1] = args[i];
+            }
+        }
 
         return cmd;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
+
+        Cmd cmd = Cmd.parseCmd(args);
+
+        if (cmd.versionFlag) {
+            System.out.println("version 0.0.1");
+        } else if (cmd.helpFlag || cmd.clazz == null) {
+            Cmd.printUsage();
+        } else {
+            startJVM(cmd);
+        }
     }
 
+    private static void startJVM(Cmd cmd) {
+        System.out.println("classpath:" + cmd.cpOption + " class:" + cmd.clazz + " args:" + Arrays.asList(cmd.args));
+    }
 
 }
