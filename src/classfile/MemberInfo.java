0238d58c9d0d297e33a0b7f75388eea5fdb08214
Michael Chow <mickey_zhouyou@yeah.net>
2018-07-02 22:15:03 +0800

    complete readMembers & readMember

--- src/classfile/MemberInfo.java
+++ src/classfile/MemberInfo.java
@@ -2,8 +2,43 @@ package classfile;
 
 public class MemberInfo {
 
+    private ConstantPool cp;
+    private char accessFlags;
+    private char nameIndex;
+    private char descriptorIndex;
+    private AttributeInfo[] attributes;
+
     public static MemberInfo[] readMembers(ClassReader reader, ConstantPool cp) {
+        char memberCount = reader.readUint16();
+        var members = new MemberInfo[memberCount];
+        for (int i = 0; i < memberCount; ++i) {
+            members[i] = readMember(reader, cp);
+        }
+        return members;
+    }
+
+    public static MemberInfo readMember(ClassReader reader, ConstantPool cp) {
+        var member = new MemberInfo();
+        member.cp = cp;
+        member.accessFlags = reader.readUint16();
+        member.nameIndex = reader.readUint16();
+        member.descriptorIndex = reader.readUint16();
+        member.attributes = AttributeInfo.readAttributes(reader, cp);
+        return member;
+    }
+
+    public char accessFlags() {
+        return this.accessFlags;
+    }
+
+    public String name() {
         // TODO
         return null;
     }
+
+    public String descriptor() {
+        // TODO
+        return null;
+    }
+
 }
