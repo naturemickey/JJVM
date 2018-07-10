622d171bce256339dae7fcf4b269f20b255dabc8
Michael Chow <mickey_zhouyou@yeah.net>
2018-07-10 15:34:24 +0800

    complete UnparsedAttribute.java‘

--- src/classfile/attr/UnparsedAttribute.java
+++ src/classfile/attr/UnparsedAttribute.java
@@ -3,8 +3,19 @@ package classfile.attr;
 import classfile.ClassReader;
 
 public class UnparsedAttribute extends AttributeInfo {
+
+    private String name;
+    private int length;
+    private byte[] info;
+
+    public UnparsedAttribute(String name, int length, byte[] info) {
+        this.name = name;
+        this.length = length;
+        this.info = info;
+    }
+
     @Override
     public void readInfo(ClassReader reader) {
-
+        this.info = reader.readBytes(this.length);
     }
 }
