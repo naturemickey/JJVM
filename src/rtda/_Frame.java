aa37a2daa9b1a40ee5f3b00954cec6ae3f4f90ad
Michael Chow <mickey_zhouyou@yeah.net>
2018-07-17 15:05:28 +0800

    complete _Frame.java

--- src/rtda/_Frame.java
+++ src/rtda/_Frame.java
@@ -3,4 +3,17 @@ package rtda;
 public class _Frame {
     private _LocalVars localVars;
     private _OperandStack operandStack;
+
+    public _Frame(int maxLocals, int maxStack) {
+        this.localVars = new _LocalVars(maxLocals);
+        this.operandStack = new _OperandStack(maxStack);
+    }
+
+    public _LocalVars localVars() {
+        return this.localVars;
+    }
+
+    public _OperandStack operandStack() {
+        return this.operandStack;
+    }
 }
