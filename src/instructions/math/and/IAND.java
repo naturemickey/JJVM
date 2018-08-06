188c8325a67855f2901fd31083bbb27351d43c03
Michael Chow <mickey_zhouyou@yeah.net>
2018-08-06 16:28:22 +0800

    complete and

--- src/instructions/math/and/IAND.java
+++ src/instructions/math/and/IAND.java
@@ -1,16 +1,15 @@
-package instructions.math.sh;
+package instructions.math.and;
 
 import instructions.base.inst.NoOperandsInstruction;
 import rtda._Frame;
 
-public class LUSHR extends NoOperandsInstruction {
+public class IAND extends NoOperandsInstruction {
     @Override
     public void execute(_Frame frame) {
         var stack = frame.operandStack();
         var v2 = stack.popInt();
-        var v1 = stack.popLong();
-        var s = v2 & 0x3f;
-        var result = v1 >>> s;
-        stack.pushLong(result);
+        var v1 = stack.popInt();
+        var result = v1 & v2;
+        stack.pushInt(result);
     }
 }
--- src/instructions/math/sh/IUSHR.java
+++ src/instructions/math/sh/IUSHR.java
@@ -1,4 +1,16 @@
 package instructions.math.sh;
 
-public class IUSHR extends ISHR {
+import instructions.base.inst.NoOperandsInstruction;
+import rtda._Frame;
+
+public class IUSHR extends NoOperandsInstruction {
+    @Override
+    public void execute(_Frame frame) {
+        var stack = frame.operandStack();
+        var v2 = stack.popInt();
+        var v1 = stack.popInt();
+        var s = v1 & 0x1f;
+        var result = v2 >>> s;
+        stack.pushInt(result);
+    }
 }
