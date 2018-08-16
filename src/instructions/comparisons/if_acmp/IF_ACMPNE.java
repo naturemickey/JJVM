4085419f6cc854d0a7aac2f60a650765d6f9e209
Michael Chow <mickey_zhouyou@yeah.net>
2018-08-16 09:45:12 +0800

    complete if_acmp

--- src/instructions/comparisons/if_acmp/IF_ACMPNE.java
+++ src/instructions/comparisons/if_acmp/IF_ACMPNE.java
@@ -3,14 +3,14 @@ package instructions.comparisons.if_acmp;
 import instructions.base.inst.BranchInstruction;
 import rtda._Frame;
 
-public class IF_ACMPEQ extends BranchInstruction {
+public class IF_ACMPNE extends BranchInstruction {
     @Override
     public void execute(_Frame frame) {
         var stack = frame.operandStack();
         var v2 = stack.popRef();
         var v1 = stack.popRef();
 
-        if (v1 == v2) {
+        if (v1 != v2) {
             branch(frame, super.offset);
         }
     }
