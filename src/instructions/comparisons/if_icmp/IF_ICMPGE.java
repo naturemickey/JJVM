9dead786f1efab3b6661bd37b57d6a992c1d9883
Michael Chow <mickey_zhouyou@yeah.net>
2018-08-16 09:38:15 +0800

    complete if_icmp

---
+++ src/instructions/comparisons/if_icmp/IF_ICMPGE.java
@@ -0,0 +1,17 @@
+package instructions.comparisons.if_icmp;
+
+import instructions.base.inst.BranchInstruction;
+import rtda._Frame;
+
+public class IF_ICMPGE extends BranchInstruction {
+    @Override
+    public void execute(_Frame frame) {
+        var stack = frame.operandStack();
+        var v2 = stack.popInt();
+        var v1 = stack.popInt();
+
+        if (v1 >= v2) {
+            branch(frame, super.offset);
+        }
+    }
+}
